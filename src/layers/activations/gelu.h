bb97ea0ff0f5cc5aacf3c3f61ed00e65c30eaa0f
subham kumar manjhi <subhamskm52@gmail.com>
2026-08-23 12:34:48 +0530

    feat(activations): added GELU activation layer

---
+++ src/layers/activations/gelu.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <Eigen/Dense>
+
+namespace lumia::layer::activation {
+    class GELU {
+    public:
+        static inline Eigen::MatrixXd forward(const Eigen::MatrixXd& x) {
+
+           return x.unaryExpr([](const double val) {
+               return 0.5f * val * (1.0f + std::erf(val / std::sqrt(2.0f)));
+            });
+        }
+    };
+}
