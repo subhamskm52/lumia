5ad213854637914f42ad136d872a2f0f50a102ed
subham kumar manjhi <subhamskm52@gmail.com>
2026-08-25 11:35:28 +0530

    feat(parameter)-refactor(linearLayer): added Parameter data class & refactored linear layer to accomodate the changes

--- include/lumia/layers/linear.h
+++ include/lumia/layers/linear.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <Eigen/Dense>
+
+#include "lumia/model/paramerter.h"
+
 namespace lumia::layers {
     class Linear {
     public:
         Linear(std::size_t in_features, std::size_t out_features);
         Eigen::MatrixXd forward(const Eigen::MatrixXd& x);
     private:
-        Eigen::MatrixXd weights;
-        Eigen::MatrixXd bias;
+        lumia::model::Parameter weights;
+        lumia::model::Parameter bias;
+
+
     };
 }
--- src/layers/linear.cpp
+++ src/layers/linear.cpp
@@ -2,11 +2,11 @@
 
 namespace lumia::layers {
     Linear::Linear(std::size_t in_features, std::size_t out_features) {
-        weights = Eigen::MatrixXd::Random(out_features, in_features);
-        bias = Eigen::MatrixXd::Random(out_features,1);
+        weights.value = Eigen::MatrixXd::Random(out_features, in_features);
+        bias.value = Eigen::MatrixXd::Random(out_features,1);
     }
 
     Eigen::MatrixXd Linear::forward(const Eigen::MatrixXd& x) {
-        return x*weights.transpose() + bias.transpose();
+        return x*weights.value.transpose() + bias.value.transpose();
     }
 }
